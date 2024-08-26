5fe8b623f8e76ff269b196b057b7977c88045cca
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-26 18:17:10 +0200

    Binary Searching

---
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -0,0 +1,44 @@
+public class BinarySearchTree {
+
+    Node Root;
+
+    public void Insert(Node node) {
+
+    }
+
+    private Node InsertHelper(Node root, Node node) {
+        return null;
+    }
+
+    public void Display() {
+
+    }
+
+    private void DisplayHelper(Node root) {
+
+    }
+
+    public boolean Search(int data) {
+        return false;
+    }
+
+    private boolean SearchHelper(Node root, int data) {
+        return false;
+    }
+
+    public void Remove(int data) {
+
+    }
+
+    public Node RemoveHelper(Node root, int data) {
+        return null;
+    }
+
+    private int Successor(Node root) {
+        return 0;
+    }
+
+    private int Predecessor(Node root) {
+        return 0;
+    }
+}
---
+++ BinarySearchTree/src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+
+    int Data;
+    Node Left;
+    Node Right;
+
+    public Node(int Data) {
+        this.Data = Data;
+    }
+}
