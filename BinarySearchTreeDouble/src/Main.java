d06c3403f3637f834684eecf246209d5cc178e1a
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-28 14:42:13 +0200

    Random values

--- BinarySearchTreeDouble/src/Main.java
+++ BinarySearchTreeDouble/src/Main.java
@@ -1,18 +1,20 @@
+import java.util.Random;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
 
         BinarySearchTree Tree1 = new BinarySearchTree();
+        Random Rand = new Random();
+        double[] Data = new double[20];
 
-        Tree1.Insert(new Node(134.3234));
-        Tree1.Insert(new Node(123.4734));
-        Tree1.Insert(new Node(197.7123));
-        Tree1.Insert(new Node(145.2078));
-        Tree1.Insert(new Node(175.6123));
-        Tree1.Insert(new Node(134.3734));
-        Tree1.Insert(new Node(174.2343));
-        Tree1.Insert(new Node(188.3634));
-        Tree1.Insert(new Node(145.3786));
-
-        Tree1.Display();
+        for (int i = 0; i < Data.length; i++) {
+            double TempValue = Rand.nextDouble() * 10000;
+            BigDecimal Round = new BigDecimal(TempValue).setScale(3, RoundingMode.HALF_UP);
+            Data[i] = Round.doubleValue();
+            Tree1.Insert(new Node(Data[i]));
+        }
+       Tree1.Display();
     }
 }
